0d0edd246c57d5ae699cc55d90483f131215fbe2
princessruthie <missruthie@gmail.com>
2016-07-11 10:10:06 -0400

    Add parcelable model for news articles.

---
+++ app/src/main/java/com/ruthiefloats/usegooglebooksapi/model/Article.java
@@ -0,0 +1,94 @@
+package com.ruthiefloats.usegooglebooksapi.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Article implements Parcelable {
+
+    public String getWebTitle() {
+        return webTitle;
+    }
+
+    public void setWebTitle(String webTitle) {
+        this.webTitle = webTitle;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getContributorWebTitle() {
+        return contributorWebTitle;
+    }
+
+    public void setContributorWebTitle(String contributorWebTitle) {
+        this.contributorWebTitle = contributorWebTitle;
+    }
+
+    private String webTitle;
+    private String webUrl;
+    private String thumbnail;
+    private String contributorWebTitle;
+
+    public Article(String webTitle, String webUrl, String thumbnail, String contributorWebTitle) {
+        this.webTitle = webTitle;
+        this.webUrl = webUrl;
+        this.thumbnail = thumbnail;
+        this.contributorWebTitle = contributorWebTitle;
+    }
+
+    public Article(){}
+
+    @Override
+    public String toString() {
+        return "Article Title :" + webTitle + "\n"
+                +"Author: " + contributorWebTitle + "\n"
+                +"Thumbnail: " + thumbnail + "\n"
+                +"WebURL: " + webUrl;
+    }
+
+    protected Article(Parcel in) {
+        webTitle = in.readString();
+        webUrl = in.readString();
+        thumbnail = in.readString();
+        contributorWebTitle = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(webTitle);
+        dest.writeString(webUrl);
+        dest.writeString(thumbnail);
+        dest.writeString(contributorWebTitle);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Article> CREATOR = new Parcelable.Creator<Article>() {
+        @Override
+        public Article createFromParcel(Parcel in) {
+            return new Article(in);
+        }
+
+        @Override
+        public Article[] newArray(int size) {
+            return new Article[size];
+        }
+    };
+}
